ea332ef33725a3abc835ee173db87ace00914456
baquedano <baquedano@unitec.edu>
2016-03-27 17:46:00 -0600

    arreglo a comments y la cagada de dennis en login haha

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,25 +13,38 @@ exports.login = {
     }
   },
   handler: function(request, reply) {
+      var entro = false;
       docente.find({email: request.payload.email, password: String(SHA3(request.payload.password))}, function(err, user){
         if(!err){
           if(user.length > 0){
             //request.auth.session.set(user[0]);
             return reply({email: user[0].email, IdUser:user[0].Id_docente,CurrentCurso:"0",Id_universidad: user[0].Id_universidad,IdTarea:null});
+            entro = true;
           }
         }
-      })
-      estudiante.find({email: request.payload.email, password: String(SHA3(request.payload.password))}, function(err, user){
-        if (!err) {
-          	if(user.length > 0){
-            //request.auth.session.set(user[0]);
-            	return reply({email: user[0].email, IdUser:user[0].Id_estudiante,CurrentCurso:"0",Id_universidad: user[0].Id_universidad,IdTarea:null});
-          	}else{
-        		var erro=boom.unauthorized('error')
-	          	return reply(erro.message);
-	        }
-        }
-   	})
+      }).then(function(query){
+        if(query.length > 0)// comprobando si la query de docente trajo algo
+          entro = true;
+        estudiante.find({email: request.payload.email, password: String(SHA3(request.payload.password))}, function(err, user){
+          if (!err) {
+            	if(user.length > 0){
+              //request.auth.session.set(user[0]);
+              	return reply({email: user[0].email, IdUser:user[0].Id_estudiante,CurrentCurso:"0",Id_universidad: user[0].Id_universidad,IdTarea:null});
+            	}else{
+                if(!entro){
+                  var erro=boom.unauthorized('error')
+                  return reply(erro.message);
+                }
+              }
+          }else{
+            var erro=boom.unauthorized('error')
+            return reply(erro.message);
+          }
+       	})//fin query a estudiante
+
+
+      })//fin then
+
   }
 }
 
